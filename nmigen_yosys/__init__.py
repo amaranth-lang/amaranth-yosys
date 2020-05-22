b91f0d57b33990a8a613969b1e3f49be68aa1bd9
whitequark <whitequark@whitequark.org>
2020-05-22 12:14:21 +0000

    [skip ci] Fix incorrect comment.

--- nmigen_yosys/__init__.py
+++ nmigen_yosys/__init__.py
@@ -1,5 +1,5 @@
 try:
-	from importlib import metadata as importlib_metadata # py3.7+ stdlib
+	from importlib import metadata as importlib_metadata # py3.8+ stdlib
 except ImportError:
-	import importlib_metadata # py3.6 shim
+	import importlib_metadata # py3.7- shim
 __version__ = importlib_metadata.version(__package__)
