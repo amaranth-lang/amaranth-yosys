a28d988a4ab54c7324d5d305b2241e340376b9d5
whitequark <whitequark@whitequark.org>
2020-08-27 13:20:00 +0000

    Derive PyPI version from Yosys makefile version, not git-describe.
    Necessary to allow nMigen to compare node versions without incurring
    the latency of running a wasmtime binary.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+import subprocess
 from setuptools import setup, find_packages
 from setuptools_scm.git import parse as parse_git
 
 
 def version():
-    yosys_git = parse_git("yosys-src")
-    if yosys_git.exact:
-        yosys_version = yosys_git.format_with("{tag}")
+    yosys_makefile_ver = subprocess.check_output([
+        "make", "-s", "-C", "yosys-src", "echo-yosys-ver"
+    ], encoding="utf-8").strip().split("+")
+    if len(yosys_makefile_ver) == 1:
+        yosys_version = yosys_makefile_ver[0]
+    elif len(yosys_makefile_ver) == 2:
+        yosys_version = f"{yosys_makefile_ver[0]}.post{yosys_makefile_ver[1]}"
     else:
-        yosys_version = yosys_git.format_with("{tag}.post{distance}")
+        assert False
 
     package_git = parse_git(".")
     package_version = package_git.format_with(".dev{distance}")
